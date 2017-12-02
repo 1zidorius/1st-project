8ef952e1cf51ba2c178fbcc97c54286ed01c5f0a
1zidorius <33282594+1zidorius@users.noreply.github.com>
2017-12-02 15:11:53 +0200

    Update skaiciuotuvas.sh

--- skaiciuotuvas.sh
+++ skaiciuotuvas.sh
@@ -1,1 +1,20 @@
 #primityvus skaiciuotuvas
+#!/bin/bash
+
+echo "Iveskite pirmaji skaiciu: "
+read sk1
+echo "Iveskite antraji skaiciu: "
+read sk2
+echo "Kokia norite atlikti operacija tarp siu skaiciu?"
+
+echo "1)Sudetis"
+echo "2)Atimtis"
+echo "3)Daugyba"
+echo "4)Dalyba"
+read ats
+
+case $ats in
+1) echo "Atsakymas: $((sk1+sk2))"
+2) echo "Atsakymas: $((sk1-sk2))"
+3) echo "Atsakymas: $((sk1*sk2))"
+4) echo "Atsakymas: $((sk1+/sk2))"
