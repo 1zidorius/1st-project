59ef7b5298d511eb92ca33571ae7fd5ea3c8a612
viliusmileris <viliusm@wix.com>
2024-04-19 14:38:40 +0300

    add retry action

---
+++ .github/scripts/retry-action.js
@@ -0,0 +1,45 @@
+const { Octokit } = require("@octokit/rest");
+
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+});
+
+async function retryAction() {
+  try {
+    const { data: pulls } = await octokit.pulls.list({
+      owner: process.env.GITHUB_REPOSITORY.split("/")[0],
+      repo: process.env.GITHUB_REPOSITORY.split("/")[1],
+      state: "open",
+    });
+
+    for (const pull of pulls) {
+      const { data: statuses } = await octokit.repos.listStatusesForRef({
+        owner: process.env.GITHUB_REPOSITORY.split("/")[0],
+        repo: process.env.GITHUB_REPOSITORY.split("/")[1],
+        ref: pull.head.sha,
+      });
+
+      console.log('statuses', JSON.stringify(statuses, null, 2))
+
+      const actionRun = statuses.find((status) => status.context === "Lock Branch");
+
+      // if (actionRun && actionRun.state === "failure") {
+      if (actionRun) {
+        // Retry your action by creating a new status with the same context and the 'pending' state
+        await octokit.repos.createCommitStatus({
+          owner: process.env.GITHUB_REPOSITORY.split("/")[0],
+          repo: process.env.GITHUB_REPOSITORY.split("/")[1],
+          sha: pull.head.sha,
+          state: "failure",
+          context: "Lock Branch",
+          description: "Retrying GitHub Action...",
+        });
+      }
+    }
+  } catch (error) {
+    console.error("Error occurred:", error);
+    process.exit(1);
+  }
+}
+
+retryAction();
