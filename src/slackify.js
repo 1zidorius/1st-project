89f4cc957aa69e0442c974a7cc1996c8eeef3254
viliusm <viliusm@wix.com>
2025-01-30 09:31:14 +0200

    test

--- src/slackify.js
+++ src/slackify.js
@@ -5,7 +5,6 @@ const path = require('path');
 
 const changelogPath = path.join(__dirname, '../CHANGELOG.md');
 const CHANGELOG = fs.readFileSync(changelogPath, 'utf8');
-
 const message = slackifyMarkdown(CHANGELOG);
 
 function splitStringifiedJSON(jsonObject, maxLength = 3000, splitChar = '•') {
@@ -37,29 +36,7 @@ function splitStringifiedJSON(jsonObject, maxLength = 3000, splitChar = '•') {
 
 const sections = splitStringifiedJSON(message);
 
-const resultOG = {
-  "blocks": [
-    {
-      "type": "section",
-      "fields": [
-        {
-          "type": "plain_text",
-          "text": "New version of Ricos has been released!"
-        }
-      ]
-    },
-    {
-      "type": "section",
-      "text": {
-        "type": "mrkdwn",
-        "text": JSON.stringify(slackifyMarkdown(CHANGELOG))
-      }
-    }
-  ]
-}
-
-
-const result = {
+const result = JSON.stringify({
   blocks: [
     {
       type: 'section',
@@ -80,12 +57,8 @@ const result = {
       };
     }),
   ],
-}
-
-console.log(JSON.stringify(resultOG, null, 2));
-console.log('-------------------');
-console.log(JSON.stringify(result, null, 2));
-
-
+});
+console.log(result);
+return result;
 
 // console.log(slackifyMarkdown(changelog));
